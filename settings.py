1e079de43cd687db7df8a4acc1fe71181228e66a
paulovf <paulovfrancisco@gmail.com>
2013-03-02 20:46:50 -0300

    Criação dos arquivos do programa de operações remotas com criptografia

---
+++ settings.py
@@ -0,0 +1,24 @@
+#coding: utf-8
+
+"""
+Modulo responsavel pelas configuracoes globais do software.
+"""
+
+# Host (Ip do Servidor), PORTA (Porta do Servidor), LISTEN (maquinas)
+HOST_NOMES = '127.0.0.1'
+HOST_FUNCOES = '127.0.0.1'
+PORTA_NOMES = 6559
+PORTA_FUNCOES = 4455
+LISTEN = 5
+
+# Tipos de Requisicoes do Cliente. Nao alterar.
+SOMA = 'SOMA'
+PRODUTO = 'PROD'
+DIVISAO = 'DIVI'
+FATORIAL = 'FATO'
+FUNCOES = 'FUNC'
+
+# Tamanho das Mensagens de requisicao.
+TAM_MSG = 6
+
+SERVIDORES = 'nomes.txt'
