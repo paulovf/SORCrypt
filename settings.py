cf09abc3ec389d1b0a4ad9e0c00e46a85de212ce
CharlesGarrocho <charles.garrocho@gmail.com>
2013-03-02 21:37:28 -0300

    corrigido: passagem de valores pelas mensagens.

--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@ Modulo responsavel pelas configuracoes globais do software.
 # Host (Ip do Servidor), PORTA (Porta do Servidor), LISTEN (maquinas)
 HOST_NOMES = '127.0.0.1'
 HOST_FUNCOES = '127.0.0.1'
-PORTA_NOMES = 6555
-PORTA_FUNCOES = 4455
+PORTA_NOMES = 6554
+PORTA_FUNCOES = 4435
 LISTEN = 5
 
 # Tipos de Requisicoes do Cliente. Nao alterar.
